54232541fb849dcd6277a52d5c5e35688bbf2cf0
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-05-27 20:17:49 -0300

    rendered component SectionNewRelease

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,13 +1,15 @@
 import { SectionCollections } from "../SectionCollections";
 import { SectionMovieSpotlight } from "../SectionMovieSpotlight";
+import { SectionNewReleases } from "../SectionNewReleases";
 
 export function Main() {
   return (
     <main>
       <SectionMovieSpotlight />
 
-      <div>
+      <div className={`md:flex md:flex-col gap-[50px]`}>
         <SectionCollections />
+        <SectionNewReleases />
       </div>
     </main>
   );
