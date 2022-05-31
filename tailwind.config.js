b436117fe683a1af2c4612694bd22dc922da360d
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-05-30 21:48:32 -0300

    added font family gilroy medium and bold

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        "gilroy-medium": ["Gilroy-Medium", "sans-serif"],
+        "gilroy-bold": ["Gilroy-Bold", "sans-serif"]
+      },
       backgroundImage: {
         "spider-men": "url('./src/assets/background-spider-men.jpg')"
       }
